ae6dd15f2bfe291bab453ed3963377cb36316b07
gilvan.dryve <gilvan.macedo@dryve.com.br>
2022-07-29 21:19:59 -0300

    first swithed banches

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // This is the theme of your application.
         //
-        // Try running your application with "flutter run". You'll see the
+        // Try running your application with "flutter run". You'll see thegi
         // application has a blue toolbar. Then, without quitting the app, try
         // changing the primarySwatch below to Colors.green and then invoke
         // "hot reload" (press "r" in the console where you ran "flutter run",
